ceccfb0986d67a510997fc4dd783d3b6ab85fb37
Mahadi Hasan <83346621+mahadi10040@users.noreply.github.com>
2021-05-06 21:35:19 +0600

    splash-done

--- app/src/main/java/com/example/renthere/SplashActivity.kt
+++ app/src/main/java/com/example/renthere/SplashActivity.kt
@@ -1,11 +1,38 @@
 package com.example.renthere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var textView:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        this.window.setFlags(WindowManager.LayoutParams.TYPE_STATUS_BAR, WindowManager.LayoutParams.TYPE_STATUS_BAR)
+
+        textView = findViewById(R.id.textView)
+        textAnimation()
+
+        moveToAuthenticationActivity()
+    }
+
+    private fun textAnimation(){
+        val fade = AnimationUtils.loadAnimation(this, R.anim.fade)
+        fade.duration = 3000
+        textView.animation = fade
+    }
+
+    private fun moveToAuthenticationActivity(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, AuthenticationActivity::class.java))
+            finish()
+        },4000)
     }
 }
