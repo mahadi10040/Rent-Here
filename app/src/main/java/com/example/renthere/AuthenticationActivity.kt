41c9b148d3978a7a4f65d701b3e47b6973eb7e30
Mahadi Hasan <83346621+mahadi10040@users.noreply.github.com>
2021-05-06 21:15:50 +0600

    initial commit

---
+++ app/src/main/java/com/example/renthere/AuthenticationActivity.kt
@@ -0,0 +1,1 @@
+package com.example.renthereimport androidx.appcompat.app.AppCompatActivityimport android.os.Bundleclass AuthenticationActivity : AppCompatActivity() {    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        setContentView(R.layout.activity_authentication)    }}
